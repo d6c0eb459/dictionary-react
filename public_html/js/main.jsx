1a3311c78d81c24aa3398adf4a3555e25bb534dd
Derek <derek@port7e.ca>
2023-08-08 20:10:03 -0400

    Simple React setup.

---
+++ public_html/js/main.jsx
@@ -0,0 +1,10 @@
+const app = document.getElementById('app');
+const root = ReactDOM.createRoot(app);
+root.render(
+    <h1>
+        Hello world!
+        <span className="material-icons">
+            favorite
+        </span>
+    </h1>
+);
